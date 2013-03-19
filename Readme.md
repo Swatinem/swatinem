237cdf62ca871720f7bbcf6096e473a458b49ba6
Arpad Borsos <arpad.borsos@googlemail.com>
2013-03-19 19:20:59 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# swatinem
+
+Just my interpretation of a static site generator.
+
+Transforms (and watches) all `.md`, `.markdown`, `.jade` and `.html` files
+inside the `articles` folder and uses the `templates/article.jade` template
+to generate a static html file inside the `generated` folder.
+
+A sorted list is passed to the `templates/index.jade` template to generate the
+`generated/index.html` file.
+
+The files can have the following metadata:
+```
+title: The Title
+author: Arpad Borsos
+time: 2013-03-19T19:30:00.000Z
+tags: this, is, currently-unused
+
+And after those two newlines is when the actual content starts
+```
+
+## Example nginx config
+
+```
+        location /news {
+                root /var/www/generated;
+                try_files /index.html @node;
+        }
+
+        location ~ ^/news/(.+)$ {
+                root /var/www/generated;
+                try_files /$1 /$1.html @node;
+        }
+```
---
+++ app.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+var Articles = require('./lib/articles');
+var articles = Articles({
+	dir: path.join(__dirname, 'articles'),
+	output: path.join(__dirname, 'generated'),
+	templates: path.join(__dirname, 'templates')
+});
+
+/*
+var express = require('express');
+var app = express();
+
+app.configure('development', function() {
+	app.use(express.logger('dev'));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('trust proxy', true);
+	app.use(app.router);
+	//app.use(express.static('generated'));
+});
+
+app.get('/news/:id', function (req, res, next) {
+	if (!(req.params.id in articles.byId))
+		return next();
+	res.header("Content-Type", "text/html; charset=utf-8");
+	res.send(articles.byId[req.params.id].generated);
+});
+
+if (!module.parent) {
+	var http = require('http');
+	var port = app.get('port');
+	var server = http.createServer(app).listen(port, '127.0.0.1', function () {
+		console.log("Express server listening on port %d", port);
+	});
+}*/
